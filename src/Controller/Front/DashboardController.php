16522fb31b7c00d142e27b67cf417ce68f75ae01
Vitaly Alyokhin <228vit@gmail.com>
2025-01-13 22:09:44 +0300

    ..

--- src/Controller/Front/DashboardController.php
+++ src/Controller/Front/DashboardController.php
@@ -6,12 +6,34 @@ use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class DashboardController extends AbstractController
 {
+
+    /**
+     * @Route("/testmail", name="front_testmail")
+     */
+    public function testMail(MailerInterface $mailer): Response
+    {
+        $senderEmail = $this->getParameter('mailer_sender_email');
+
+        $email = (new Email())
+            ->from($senderEmail)
+            ->to('228vit@gmail.com')
+            ->priority(Email::PRIORITY_LOW)
+            ->subject('[WINE] Mailer')
+            ->text('Sending emails is fun again!')
+        ;
+
+        $mailer->send($email);
+
+        return new Response('sent...');
+    }
 
     /**
      * @Route("/cabinet", name="cabinet")
